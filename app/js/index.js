34e4d562e38834b1320cb9c0f29fde44c3dacd05
Richard Ramos <info@richardramos.me>
2018-10-03 09:36:13 -0400

    Adding React

--- app/js/index.js
+++ app/js/index.js
@@ -1,6 +1,19 @@
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
 import EmbarkJS from 'Embark/EmbarkJS';
-
-// import your contracts
-// e.g if you have a contract named SimpleStorage:
+import web3 from 'Embark/web3'
 //import SimpleStorage from 'Embark/contracts/SimpleStorage';
+class App extends React.Component {
 
+    constructor(props) {
+      super(props);
+    }
+  
+    render(){
+     return <h1>Hello World</h1>
+        
+    }
+  }
+  
+
+ReactDOM.render(<App></App>, document.getElementById('app'));
